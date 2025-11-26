1b1bed8f128b6fb54d0bf825b47f0098f34092a9
Lil Mosey <rainisstreaming@gmail.com>
2025-11-26 13:32:28 -0800

    Added back server folder to run locally

--- src/components/pages/Reservation.jsx
+++ src/components/pages/Reservation.jsx
@@ -17,7 +17,7 @@ function Reservation() {
       e.preventDefault();
 
       try {
-        const response = await fetch("/api/reservations", {
+        const response = await fetch("http://localhost:5000/api/reservations", {
           method: "POST",
           headers: { "Content-Type": "application/json"},
           body: JSON.stringify(formData),
